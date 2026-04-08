cb882a79eaa77a11d7bb0a57994afcc352324b20
Johannes Graf <63838747+Graf-J@users.noreply.github.com>
2026-04-08 16:43:10 -0500

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        # Initialize Char Map
+        char_map = defaultdict(int)
+        for char in t:
+            char_map[char] += 1
+
+        min_l, min_r = float("-inf"), float("inf")
+        l = 0
+        for r in range(len(s)):
+            if s[r] in char_map:
+                char_map[s[r]] -= 1
+
+            if all(x <= 0 for x in char_map.values()):
+                while l <= r and all(x <= 0 for x in char_map.values()):
+                    if s[l] in char_map:
+                        char_map[s[l]] += 1
+                    if (r - l) < (min_r - min_l):
+                        min_l, min_r = l, r
+                    l += 1
+
+        if min_l >= 0 and min_r < len(s):
+            return s[min_l : min_r + 1]
+        return ""
+
+            
