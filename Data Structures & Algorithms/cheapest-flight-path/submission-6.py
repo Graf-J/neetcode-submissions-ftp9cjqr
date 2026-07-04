2cfe209eded427f76da2c025a6e3f76064820d74
Johannes Graf <63838747+Graf-J@users.noreply.github.com>
2026-07-04 11:56:15 +0200

    Add: cheapest-flight-path - submission-6

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        dist = [float("inf")] * n
+        dist[src] = 0
+        for _ in range(k + 1):
+            dist_tmp = dist.copy()
+            for u, v, d in flights: # u = Source; v = Destination; d = Distance
+                if dist[u] + d < dist_tmp[v]:
+                    dist_tmp[v] = dist[u] + d
+
+            dist = dist_tmp
+
+        return -1 if dist[dst] == float("inf") else dist[dst]
+
