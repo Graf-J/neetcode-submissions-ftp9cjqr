e1b87e8a3d19e3aa0ae4ae95865504daea3ca9d0
Johannes Graf <63838747+Graf-J@users.noreply.github.com>
2026-08-14 17:52:37 +0200

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.py
@@ -0,0 +1,44 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        # Base-Case
+        if len(hand) % groupSize:
+            return False
+
+        num_freq = {}
+        for card in hand:
+            num_freq[card] = 1 + num_freq.get(card, 0)
+        
+        heap = list(num_freq.keys())
+        heapq.heapify(heap)
+        while heap:
+            start_card = heap[0]
+            for i in range(start_card, start_card + groupSize):
+                if i not in num_freq:
+                    return False
+                num_freq[i] -= 1
+                if num_freq[i] < 0:
+                    return False
+                if num_freq[i] == 0 and heapq.heappop(heap) != i:
+                    return False
+
+        return True
+
+
+
+                
+
+
+
+# Time-Complexity:  O(N)
+# Space-Complexity: O(N)
+
+
+# [1,2,2,3,3,4,4,5]
+
+# {
+#     1: 1,
+#     2: 2,
+#     3: 2,
+#     4: 2,
+#     5: 1
+# }
